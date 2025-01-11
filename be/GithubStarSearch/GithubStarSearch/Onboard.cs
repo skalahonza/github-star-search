28d6fd99437735c670a76082f8a12d9677783943
Jan Skála <j.skala@hotmail.com>
2025-01-11 10:40:01 +0100

    Add backend

---
+++ be/GithubStarSearch/GithubStarSearch/Onboard.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+
+namespace GithubStarSearch;
+
+public class Onboard(ILogger<Onboard> logger)
+{
+    public record OnboardUser(string Username);
+    
+    public record UserOnboarded(string Username);
+    
+    [Function("Onboard")]
+    [OpenApiOperation(operationId: "Run", tags: ["onboard"])]
+    [OpenApiRequestBody("application/json", typeof(OnboardUser), Description = "User data", Required = true)]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(UserOnboarded), Description = "The OK response")]
+    public async Task<UserOnboarded> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req, [FromBody] OnboardUser user)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request");
+        logger.LogInformation("User {Username} onboarded successfully", user.Username);
+        return new UserOnboarded(user.Username);
+    }
+
+}
---
+++ be/GithubStarSearch/GithubStarSearch/Repository.cs
@@ -0,0 +1,15 @@
+﻿namespace GithubStarSearch;
+
+public class Repository
+{
+    public required string Slug { get; init; }
+    public required string Owner { get; init; }
+
+    /// <summary>
+    /// Person who added this repository to their starred list.
+    /// </summary>
+    public required string StarredBy { get; init; }
+
+    public required string Title { get; init; }
+    public required string Description { get; init; }
+}
