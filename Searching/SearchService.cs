474031b3ad54f95088e71a86b54338e013172a1d
Jan Skála <j.skala@hotmail.com>
2025-01-12 09:01:12 +0100

    Improve UI

--- Searching/SearchService.cs
+++ Searching/SearchService.cs
@@ -15,9 +15,9 @@ public class SearchService(
     public async Task IndexRepositories(IEnumerable<Repository> repositories)
     {
         var index = client.Index(searchOptions.Value.RepositoriesIndexName);
-        
+
         var task = await index.AddDocumentsAsync(repositories);
-        
+
         var info = await index.WaitForTaskAsync(task.TaskUid);
         if (info.Status != TaskInfoStatus.Succeeded)
         {
@@ -34,42 +34,46 @@ public class SearchService(
             logger.LogInformation("Indexing succeeded in {Duration}", info.Duration);
         }
     }
-    
-    public async Task<IReadOnlyCollection<FormattedSearchableRepository>> SearchRepositories(string starredBy, string term)
+
+    public async Task<IReadOnlyCollection<Repository>> SearchRepositories(string starredBy,
+        string term)
     {
         await MakeSureIndexExists();
         await SetupIndex();
-        
+
         var index = client.Index(searchOptions.Value.RepositoriesIndexName);
         var searchFilterConditions = new List<string>();
-        
+
         if (!string.IsNullOrEmpty(starredBy))
         {
             searchFilterConditions.Add($"{nameof(Repository.StarredBy).ToCamelCase()} = {starredBy}");
         }
-        
+
         // Set the query with filter and additional settings
         var searchQuery = new SearchQuery
         {
             Filter = searchFilterConditions,
-            // AttributesToHighlight = [
-            //     nameof(Repository.Slug).ToCamelCase(),
-            //     nameof(Repository.Owner).ToCamelCase(),
-            //     nameof(Repository.Description).ToCamelCase()],
-            // AttributesToCrop = [nameof(Repository.Description).ToCamelCase()],
-            // CropLength = searchSettings.Value.CropLength
+            AttributesToHighlight =
+            [
+                nameof(Repository.Slug).ToCamelCase(),
+                nameof(Repository.Owner).ToCamelCase(),
+                nameof(Repository.Description).ToCamelCase(),
+                nameof(Repository.Readme).ToCamelCase()
+            ],
+            AttributesToCrop = [nameof(Repository.Description).ToCamelCase(), nameof(Repository.Readme).ToCamelCase()],
+            CropLength = searchOptions.Value.CropLength
         };
 
         var results = await index.SearchAsync<FormattedSearchableRepository>(term, searchQuery);
-        return results.Hits ?? [];
+        return results.Hits?.Select(x => x.Formatted ?? x).ToList() ?? [];
     }
-    
+
     public Task<ResourceResults<IEnumerable<Repository>>> GetRepositories()
     {
         var index = client.Index(searchOptions.Value.RepositoriesIndexName);
         return index.GetDocumentsAsync<Repository>();
     }
-    
+
     public async Task UpdateRepositories(IEnumerable<Repository> repositories)
     {
         var index = client.Index(searchOptions.Value.RepositoriesIndexName);
@@ -100,9 +104,11 @@ public class SearchService(
     {
         var indexes = await client.GetAllIndexesAsync();
         if (!indexes.Results.Any(index =>
-                string.Equals(index.Uid, searchOptions.Value.RepositoriesIndexName, StringComparison.OrdinalIgnoreCase)))
+                string.Equals(index.Uid, searchOptions.Value.RepositoriesIndexName,
+                    StringComparison.OrdinalIgnoreCase)))
         {
-            logger.LogInformation("Index {IndexName} does not exist. Creating one", searchOptions.Value.RepositoriesIndexName);
+            logger.LogInformation("Index {IndexName} does not exist. Creating one",
+                searchOptions.Value.RepositoriesIndexName);
             await client.CreateIndexAsync(searchOptions.Value.RepositoriesIndexName, searchOptions.Value.PrimaryKey);
             logger.LogInformation("Index {IndexName} created", searchOptions.Value.RepositoriesIndexName);
         }
